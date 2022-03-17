2876e2b38de7664b7afd6e0f05981a32e0a141e0
savarapu manohar joshi <83489094+Manohar-mj@users.noreply.github.com>
2022-03-17 11:31:34 +0530

    Create if-else.py

---
+++ if-else.py
@@ -0,0 +1,21 @@
+#!/bin/python
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(raw_input().strip())
+    if n%2 !=0:
+        print("Weird")
+    else:
+        if n in range(2,5):
+            print("Not Weird")
+        if n in range(6,20):
+            print("Weird")
+        if n>20:
+            print("Not Weird")
