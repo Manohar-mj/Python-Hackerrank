c95276880156995af90d9c13825f5297f920000a
savarapu manohar joshi <83489094+Manohar-mj@users.noreply.github.com>
2022-03-17 11:34:31 +0530

    Update if-else.py

--- if-else.py
+++ if-else.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     else:
         if n in range(2,5):
             print("Not Weird")
-        if n in range(6,20):
+        if n in range(6,21):
             print("Weird")
         if n>20:
             print("Not Weird")
